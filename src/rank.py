3e3261ce9060624fe01cb30579bba11b565f8231
jaswanth7671 <106313101+jaswanth7671@users.noreply.github.com>
2026-03-03 16:54:14 -0600

    Update rank.py

--- src/rank.py
+++ src/rank.py
@@ -1,1 +1,19 @@
+def rank_jobs(jobs):
 
+    ranked = []
+
+    for job in jobs:
+        score = 0
+
+        if job.skills:
+            if "Python" in job.skills:
+                score += 2
+
+            if "MLflow" in job.skills:
+                score += 1
+
+        ranked.append((score, job))
+
+    ranked.sort(reverse=True, key=lambda x: x[0])
+
+    return [job for score, job in ranked]
