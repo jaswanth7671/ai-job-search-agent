33565bdb7746c6f200084711f718202dc4623b39
jaswanth7671 <106313101+jaswanth7671@users.noreply.github.com>
2026-03-03 16:53:56 -0600

    Implement job filtering for FAANG companies
    Add a function to filter out jobs from FAANG companies

--- src/filter.py
+++ src/filter.py
@@ -1,1 +1,12 @@
+FAANG = ["Google","Amazon","Meta","Apple","Netflix","Microsoft"]
 
+def filter_jobs(jobs):
+
+    filtered = []
+
+    for job in jobs:
+        if job.company in FAANG:
+            continue
+        filtered.append(job)
+
+    return filtered
